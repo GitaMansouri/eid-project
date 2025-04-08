f3965052c06eb1aa8f7fe9f9c102774d5bf44080
GitaMansouri <gita.mansouri.84@gmail.com>
2025-04-08 10:16:50 -0700

    copy test code in main then fix bugs

--- example/Human.java
+++ example/Human.java
@@ -1,7 +1,8 @@
 package example;
 import db.Entity;
+import db.exception.InvalidEntityException;
 
-public class Human extends Entity implements Cloneable {
+public class Human extends Entity {
     public static String name;
     public static int age;
     public Human(String name, int age) {
@@ -20,6 +21,18 @@ public class Human extends Entity implements Cloneable {
         @Override
         public int getEntityCode() {
             return HUMAN_ENTITY_CODE;
+    }
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Human)) {
+            throw new IllegalArgumentException("Input type must be Human");
+        }
+        if (age < 0) {
+            throw new InvalidEntityException("Age can not be negative");
+        }
+        if (name == null){
+            throw new InvalidEntityException("Name can not be empty");
+        }
     }
 
 }
